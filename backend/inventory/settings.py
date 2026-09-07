919900d6c5b6205de590515905a442c061a623b9
nick <nick@penguin>
2026-09-08 07:30:50 +1000

    fixing so only runs postgres

--- backend/inventory/settings.py
+++ backend/inventory/settings.py
@@ -79,26 +79,18 @@ WSGI_APPLICATION = "inventory.wsgi.application"
 # ------------------------------------------------------------------------------
 # Database
 # ------------------------------------------------------------------------------
-if os.environ.get("DB_NAME"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
-# ------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
 # PIN Login Settings
 # ------------------------------------------------------------------------------
 PIN_LOGIN_USERNAME = os.environ.get("PIN_LOGIN_USERNAME", "DistrictCache")
