7a2cd3c7e811ee35e34439044e963f846974f0bc
tancredt <nicholasrperkins@gmail.com>
2026-09-23 15:12:06 +1000

    something superficial

--- backend/stuff/serializers.py
+++ backend/stuff/serializers.py
@@ -300,6 +300,7 @@ class CylinderSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Cylinder
+        fields = "__all__"
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
